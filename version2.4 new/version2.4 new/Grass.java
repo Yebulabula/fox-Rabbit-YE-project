2ad1539b32508bb20f8c78e932e231d3a041a1f2
Yebulabula <47183066+Yebulabula@users.noreply.github.com>
2019-02-12 12:56:03 +0000

    version2.5
    mis

---
+++ version2.4 new/version2.4 new/Grass.java
@@ -0,0 +1,99 @@
+import java.util.*;
+/**
+ * 在这里给出对类 grass 的描述。
+ * 
+ * @作者（你的名字）
+ * @版本（一个版本号或者一个日期）
+ */
+public class Grass extends Plant
+{
+    // 实例变量 - 用你自己的变量替换下面的例子
+    private static final int EAT_AGE = 3;
+    private static final int MAX_AGE = 15;
+    
+    private static final double BREEDING_PROBABILITY = 0.3;
+    private static final int WATER_LEVEL = 50;
+    
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 2;
+    // A shared random number generator to control breeding.
+    private static final Random rand = Randomizer.getRandom();
+
+    private int age;
+    private int water;
+    /**
+     * 类 grass 的对象的构造函数
+     */
+    public Grass(boolean randomAge, Field field, Location location,boolean Eatable)
+    {
+        super(field, location);
+        age = 0;
+        water = WATER_LEVEL;
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+            water = rand.nextInt(WATER_LEVEL);
+            if(age > EAT_AGE)
+            {
+                canEat();
+            }
+        }
+    }
+    public void act_Sunny(List<Plant> newGrass)
+    {
+        incrementAge();
+        incrementWater();
+        if(isAlive()&&water>=0.4*(WATER_LEVEL)) {
+            giveBirth(newGrass);            
+        }
+        else{}    
+    }
+    public void act_Rainy(List<Plant> newGrass)
+    {
+        water = WATER_LEVEL;
+        incrementAge();
+        incrementWater();
+        if(isAlive()) {}
+    }
+    private void incrementAge()
+    {
+        age++;
+        if(age > EAT_AGE){
+            canEat();
+        }
+        if(age > MAX_AGE) {
+            setDead();
+        }
+    }
+        private void incrementWater()
+    {
+        water--;
+        if(water <= 0) {
+            setDead();
+        }
+    }
+    protected int getAge()
+    {
+        return age;
+    }  
+        private void giveBirth(List<Plant> newGrass)
+    {
+        // New foxes are born into adjacent locations.
+        // Get a list of adjacent free locations.
+        Field field = getField();
+        List<Location> free = field.getFreeAdjacentLocations(getLocation());
+        int births = breed();
+        for(int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Grass young = new Grass(false, field, loc,false);
+            newGrass.add(young);
+        }
+    }
+    private int breed()
+    {
+        int births = 0;
+        if(rand.nextDouble() <= BREEDING_PROBABILITY) {
+            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        }
+        return births;
+    }
+}
---
+++ version2.4 new/version2.4 new/Simulator.java
@@ -0,0 +1,283 @@
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.awt.Color;
+
+/**
+ * A simple predator-prey simulator, based on a rectangular field
+ * containing rabbits and foxes.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public class Simulator
+{
+    // Constants representing configuration information for the simulation.
+    // The default width for the grid.
+    private static final int DEFAULT_WIDTH = 120;
+    // The default depth of the grid.
+    private static final int DEFAULT_DEPTH = 80;
+    // The probability that a fox will be created in any given grid position.
+    private static final double FOX_CREATION_PROBABILITY = 0.02;
+    // The probability that a rabbit will be created in any given grid position.
+    private static final double RABBIT_CREATION_PROBABILITY = 0.1;   
+    private static final double COW_CREATION_PROBABILITY= 0.05;
+    private static final double GRASS_CREATION_PROBABILITY = 0.5;    
+    private static final double TIGER_CREATION_PROBABILITY = 0.01;   
+
+    // List of animals in the field.
+    private List<Animal> animals;
+    private String currentWeather;
+    private Weather weather;
+    private List<Plant> plants;
+    // The current state of the field.
+    private Field field;
+    // The current step of the simulation.
+    private int step;
+    // A graphical view of the simulation.
+    private SimulatorView view;
+
+    /**
+     * Construct a simulation field with default size.
+     */
+    public Simulator()
+    {
+        this(DEFAULT_DEPTH, DEFAULT_WIDTH);
+    }
+
+    /**
+     * Create a simulation field with the given size.
+     * @param depth Depth of the field. Must be greater than zero.
+     * @param width Width of the field. Must be greater than zero.
+     */
+    public Simulator(int depth, int width)
+    {
+        if(width <= 0 || depth <= 0) {
+            System.out.println("The dimensions must be greater than zero.");
+            System.out.println("Using default values.");
+            depth = DEFAULT_DEPTH;
+            width = DEFAULT_WIDTH;
+        }
+
+        animals = new ArrayList<>();
+        plants = new ArrayList<>();
+        field = new Field(depth, width);
+        currentWeather = getWeather();
+
+        // Create a view of the state of each location in the field.
+        view = new SimulatorView(depth, width);
+        view.setColor(Rabbit.class, Color.ORANGE);
+        view.setColor(Fox.class, Color.BLUE);
+        view.setColor(Tiger.class, Color.RED);
+        view.setColor(Grass.class, Color.GREEN);
+        view.setColor(Cow.class,Color.lightGray);
+        // Setup a valid starting point.
+        reset();
+    }
+
+    protected String getWeather()
+    {
+        weather= new Weather();
+        currentWeather=weather.random_weather();
+        return currentWeather;
+    }
+
+    /**
+     * Run the simulation from its current state for a reasonably long period,
+     * (4000 steps).
+     */
+    public void runLongSimulation()
+    {
+        simulate(4000);
+    }
+    //
+    /**
+     * Run the simulation from its current state for the given number of steps.
+     * Stop before the given number of steps if it ceases to be viable.
+     * @param numSteps The number of steps to run for.
+     */
+    public void simulate(int numSteps)
+    {
+        for(int step = 1; step <= numSteps && view.isViable(field); step++) {
+            simulateOneStep();
+            // delay(60);   // uncomment this to run more slowly
+        }
+    }
+
+    /**
+     * Run the simulation from its current state for a single step.
+     * Iterate over the whole field updating the state of each
+     * fox and rabbit.
+     */
+    public void simulateOneStep()
+    {
+        step++;
+
+        // Provide space for newborn animals.
+        List<Animal> newAnimals = new ArrayList<>();   
+        List<Plant> newPlants = new ArrayList<>();
+        // Let all rabbits act.
+        if(step%2==0)
+        {
+            if(currentWeather.equals("Sunny"))
+            {
+                for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) {
+                    Animal animal = it.next();
+                    animal.act_night(newAnimals);
+                    if(!animal.isAlive()) {
+                        it.remove();
+                    }
+                }
+                for(Iterator<Plant> it = plants.iterator(); it.hasNext(); ) {
+                    Plant plant = it.next();
+                    if(!plant.isAlive()) {
+                        plant.act_Sunny(newPlants);
+                        it.remove();
+                    }
+                }
+                currentWeather = getWeather();
+            }
+            else if(currentWeather.equals("Rainy"))
+            {
+
+                for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) {
+                    Animal animal = it.next();
+                    animal.act_night(newAnimals);
+                    if(!animal.isAlive()) {
+                        it.remove();
+                    }
+                }
+                for(Iterator<Plant> it = plants.iterator(); it.hasNext(); ) {
+                    Plant plant = it.next();
+                    if(!plant.isAlive()) {
+                        plant.act_Rainy(newPlants);
+                        it.remove();
+                    }
+                }
+                currentWeather = getWeather();
+            }
+        }
+        else if(step%2==1)
+        {
+            if(currentWeather.equals("Rainy")){
+                for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) {
+                    Animal animal = it.next();
+                    animal.act(newAnimals);
+                    if(!animal.isAlive()) {
+                        it.remove();
+                    }
+                }
+                for(Iterator<Plant> it = plants.iterator(); it.hasNext(); ) {
+                    Plant plant = it.next();
+                    plant.act_Rainy(newPlants);
+                    if(!plant.isAlive()) {
+                        it.remove();
+                    }
+                }
+            }
+            else if(currentWeather.equals("Sunny")){
+                for(Iterator<Animal> it = animals.iterator(); it.hasNext(); ) {
+                    Animal animal = it.next();
+                    animal.act(newAnimals);
+                    if(!animal.isAlive()) {
+                        it.remove();
+                    }
+                }
+                for(Iterator<Plant> it = plants.iterator(); it.hasNext(); ) {
+                    Plant plant = it.next();
+                    plant.act_Sunny(newPlants);
+                    if(!plant.isAlive()) {
+                        it.remove();
+                    }
+                }
+            }
+        }
+        // Add the newly born foxes and rabbits to the main lists.
+        animals.addAll(newAnimals);
+        plants.addAll(newPlants);
+
+        view.showStatus(step, field);
+    }
+
+    /**
+     * Reset the simulation to a starting position.
+     */
+    public void reset()
+    {
+        step = 0;
+        animals.clear();
+        plants.clear();
+        populate();
+
+        // Show the starting state in the view.
+        view.showStatus(step, field);
+    }
+
+    /**
+     * Randomly populate the field with foxes and rabbits.
+     */
+    private void populate()
+    {
+        Random rand = Randomizer.getRandom();
+        field.clear();
+
+        for(int row = 0; row < field.getDepth(); row++) {
+            for(int col = 0; col < field.getWidth(); col++) {
+                if(rand.nextDouble() <= FOX_CREATION_PROBABILITY) {
+                    Gender gender= new Gender();
+                    gender.get_gender();
+                    Location location = new Location(row, col);
+                    Fox fox = new Fox(true, field, location,"");
+                    fox.setSex();
+                    animals.add(fox);     
+                }
+                else if(rand.nextDouble() <= RABBIT_CREATION_PROBABILITY) {
+                    Gender gender= new Gender();
+                    gender.get_gender();
+                    Location location = new Location(row, col);
+                    Rabbit rabbit = new Rabbit(true, field, location,""); 
+                    rabbit.setSex();
+                    animals.add(rabbit);
+                }
+                else if(rand.nextDouble() <= GRASS_CREATION_PROBABILITY) {
+                    Location location = new Location(row, col);
+                    Grass grass = new Grass(true, field, location,false);
+                    plants.add(grass);     
+                }
+                else if(rand.nextDouble() <= COW_CREATION_PROBABILITY) {
+                    Gender gender= new Gender();
+                    gender.get_gender();
+                    Location location = new Location(row, col);
+                    Cow cow = new Cow(true, field, location,""); 
+                    cow.setSex();
+                    animals.add(cow);
+                }
+                else if(rand.nextDouble() <= TIGER_CREATION_PROBABILITY) {
+                    Gender gender= new Gender();
+                    gender.get_gender();
+                    Location location = new Location(row, col);
+                    Tiger tiger = new Tiger(true, field, location,""); 
+                    tiger.setSex();
+                    animals.add(tiger);
+                }
+                // else leave the location empty.
+            }
+        }
+
+    }
+
+    /**
+     * Pause for a given time.
+     * @param millisec  The time to pause for, in milliseconds
+     */
+    private void delay(int millisec)
+    {
+        try {
+            Thread.sleep(millisec);
+        }
+        catch (InterruptedException ie) {
+            // wake up
+        }
+    }
+}
